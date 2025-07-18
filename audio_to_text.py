ca2ba0cfee04ffeacef42dee5a844a373150e5a6
Aashish Sekar <107553285+aashishops@users.noreply.github.com>
2025-07-18 17:00:25 +0530

    Update audio_to_text.py

--- audio_to_text.py
+++ audio_to_text.py
@@ -1,14 +1,23 @@
-import whisper
-import tempfile
 import os
+from groq import Groq
+import tempfile
+import dotenv
+dotenv.load_dotenv()
 
-model = whisper.load_model("tiny")
-
-def transcribe_audio(audio_bytes):
-    with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as temp_audio:
+def transcribe_audio_groq(audio_bytes):
+    # Save audio to temp
+    with tempfile.NamedTemporaryFile(delete=False, suffix='.wav') as temp_audio:
         temp_audio.write(audio_bytes)
         temp_path = temp_audio.name
 
-    result = model.transcribe(temp_path)
+    # Init Groq client
+    client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+    with open(temp_path, "rb") as file:
+        transcription = client.audio.transcriptions.create(
+            file=file,
+            model="whisper-large-v3-turbo",
+            response_format="json",  
+            language="en"
+        )
     os.remove(temp_path)
-    return result["text"]
+    return transcription.text  
